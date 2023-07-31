0d878059c1115595e7dab3435f731644bd90995a
Katse <etymonym-katse@proton.me>
2023-07-31 09:59:01 -0700

    Solved P6

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,10 @@ fn p5(upper: usize) -> usize {
 
 }
 
+fn p6(upper: usize) -> usize {
+    (1..upper+1).sum::<usize>().pow(2) - (1..upper+1).map( |x: usize| x.pow(2) ).sum::<usize>()
+}
+
 fn main() {
 
     let args: Vec<String> = env::args().collect();
@@ -129,6 +133,12 @@ fn main() {
                 5 => {
                     match argument.parse::<usize>() {
                         Ok(arg) => println!("{}",p5(arg)),
+                        Err(_) => error!(num),
+                    }
+                }
+                6 => {
+                    match argument.parse::<usize>() {
+                        Ok(arg) => println!("{}",p6(arg)),
                         Err(_) => error!(num),
                     }
                 }
